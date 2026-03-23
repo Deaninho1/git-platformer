f4d6f79b5ac1c70b7daa109c6eb07424498c6013
Dean <deancannavacciuolo352@gmail.com>
2026-03-23 10:15:07 +0100

    final game commit

---
+++ Assets/scripts/PauseScript.cs
@@ -0,0 +1,26 @@
+using System.ComponentModel;
+using UnityEngine;
+
+public class PauseScript : MonoBehaviour
+{
+   public GameObject container;
+    void Update()
+    {
+       if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            container.SetActive(true);
+            Time.timeScale = 0;
+        } 
+    }
+
+    public void resumeButton()
+    {
+       container.SetActive(false);
+        Time.timeScale = 1; 
+    }
+
+    public void MainMenuButton()
+    {
+        UnityEngine.SceneManagement.SceneManager.LoadScene("");
+    }
+}
